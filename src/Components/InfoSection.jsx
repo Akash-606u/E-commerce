7cc5a86a59a910a830377e75ba33e50e80fcd942
Akash-606u <akash606u@gmail.com>
2025-05-26 18:32:42 +0530

    E-commerce

---
+++ src/Components/InfoSection.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { FaHeadset, FaLock, FaMoneyBillWave, FaShippingFast, FaTag } from 'react-icons/fa'
+
+
+const infosection = () => {
+    const infoItems = [
+        {
+         icon: <FaShippingFast className='text-3xl text-red-600'/>,
+         title: 'Free shipping',
+         description: 'Get your order delivered with no extra cost',
+        },
+        {
+            icon: <FaHeadset className='text-3xl text-red-600'/>,
+            title: 'support 24/7',
+            description:'We are here you to assist anytime',
+        },
+        {
+            icon: <FaMoneyBillWave className='text-3xl text-red-600'/>,
+            title: '100% money back',
+            description: 'full refund if you are not satisfied',
+        },
+        {
+            icon: <FaLock className='text-3xl text-red-600'/>,
+            title: 'Payment secure',
+            description: 'Your payment information is safe with us',
+        },
+        {
+            icon: <FaTag className='text-3xl text-red-600'/>,
+            title: 'Discount',
+            description: 'Enjoy the best prices on our product',
+        },
+    ];
+  return (
+    <div className='bg-white pb-8 pt-12'>
+      <div className='contaier mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
+     { infoItems.map ((item,index) => (
+        <div key={index} className='flex flex-col items-center text-center p-4 border rounded-lg shadow transform transition-transform duration-300 hover:scale-105 cursor-pointer '>
+            {item.icon}
+            <h3 className='mt-4 text-xl font-semibold'>{item.title}</h3>
+            <p className='mt-2 text-gray-600'>{item.description}</p>
+        </div>
+
+
+     ))}
+      </div>
+    </div>
+  )
+}
+
+export default infosection
